e8115d04da2c66679db398ef24869102ad13db5b
이주환 <wnghks145@gmail.com>
2025-02-21 16:38:57 +0900

    [Feat] 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/home";
+import Layout from "./components/layout";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <Login />,
+  },
+
+  { path: "/create-account",
+    element: <CreateAccount />
+  }
+  
+]);
+
 function App() {
   return (
-    <></>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
