5200b6bde57dbae5d6b3a0f5ee1848242eab3c3b
이주환 <wnghks145@gmail.com>
2025-04-01 20:07:39 +0900

    [Chore] Firebase 프로젝트 생성

---
+++ src/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8QfQrWsCHVv8AR8fQQumRHq078GZPZ9g",
+  authDomain: "x-reloaded-94ef8.firebaseapp.com",
+  projectId: "x-reloaded-94ef8",
+  storageBucket: "x-reloaded-94ef8.firebasestorage.app",
+  messagingSenderId: "748800543856",
+  appId: "1:748800543856:web:32fc222cb10c8dfebe38b5"
+};
+
+const app = initializeApp(firebaseConfig);
