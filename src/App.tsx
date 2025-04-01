33ee2c7dcf1ebd83127b60873bba51eefe1f6bc4
이주환 <wnghks145@gmail.com>
2025-04-01 18:11:52 +0900

    [Feat] styled-components GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,53 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./routes/home";
 import Layout from "./components/layout";
+import Home from "./routes/home";
 import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: <Layout/>,
     children: [
       {
         path: "",
-        element: <Home />,
+        element: <Home/>
       },
       {
         path: "profile",
-        element: <Profile />,
-      },
-    ],
+        element: <Profile/>
+      }
+    ]
   },
-
   {
     path: "/login",
-    element: <Login />,
+    element: <Login />
   },
-
-  { path: "/create-account",
+  {
+    path: "/create-account",
     element: <CreateAccount />
   }
-  
 ]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 function App() {
   return (
     <>
+    <GlobalStyles />
       <RouterProvider router={router} />
     </>
   );
